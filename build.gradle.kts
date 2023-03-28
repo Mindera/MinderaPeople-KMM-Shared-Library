03ea9b60edce322345063110bf7811b355aa5c53
Dani Camelo <dani.camelo@mindera.com>
2023-03-28 13:11:51 +0100

    MPA-79: Changing AGP version to 7.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.library").version("7.4.1").apply(false)
+    id("com.android.library").version("7.3.1").apply(false)
     kotlin("multiplatform").version("1.8.0").apply(false)
 }
 
