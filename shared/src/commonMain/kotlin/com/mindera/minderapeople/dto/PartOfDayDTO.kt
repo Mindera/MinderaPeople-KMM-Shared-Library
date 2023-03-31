99b3fb117f1672bce7192d043beb4e8acd4c6478
André Santos <andre.santos@mindera.com>
2023-03-31 15:31:01 +0100

    feat: Implemented "get all parts of the day" feature

--- shared/src/commonMain/kotlin/com/mindera/minderapeople/dto/PartOfDayDTO.kt
+++ shared/src/commonMain/kotlin/com/mindera/minderapeople/dto/PartOfDayDTO.kt
@@ -1,6 +1,12 @@
 package com.mindera.minderapeople.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PartOfDayDTO(val aux:String)
+data class PartOfDayDTO(
+    val id: String,
+    val description: String,
+    @SerialName("icon_name")
+    val iconName: String
+)
--- shared/src/commonMain/kotlin/com/mindera/minderapeople/repository/DefaultData.kt
+++ shared/src/commonMain/kotlin/com/mindera/minderapeople/repository/DefaultData.kt
@@ -2,5 +2,10 @@ package com.mindera.minderapeople.repository
 
 object DefaultData {
 
+    //API
+    const val BASE_URL = "http://localhost:3000/api"
+
+    //Error messages
     const val INVALID_USER_ID = "Invalid User ID format"
+
 }
