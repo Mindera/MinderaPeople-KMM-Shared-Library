fed8a68dc42f9dc221e0a07def93a7c0a826a556
Dani Camelo <dani.camelo@mindera.com>
2023-04-04 14:45:23 +0100

    implemented creating new event

--- shared/src/commonMain/kotlin/com/mindera/minderapeople/apiclient/EventApiClientImpl.kt
+++ shared/src/commonMain/kotlin/com/mindera/minderapeople/apiclient/EventApiClientImpl.kt
@@ -2,6 +2,9 @@ package com.mindera.minderapeople.apiclient
 
 import com.mindera.minderapeople.apiclient.interfaces.IEventApiClient
 import com.mindera.minderapeople.dto.EventDTO
+import com.mindera.minderapeople.dto.PartOfDayDTO
+import com.mindera.minderapeople.dto.PolicyDTO
+import com.mindera.minderapeople.dto.ProjectDTO
 import io.ktor.client.call.*
 import io.ktor.client.engine.*
 import io.ktor.client.request.*
@@ -26,11 +29,46 @@ class EventApiClientImpl(engine: HttpClientEngine): IEventApiClient {
     }
 
     override suspend fun getEventByPolicy(userId: String, policyId: String): Result<List<EventDTO>> {
-        return try{
+        return try {
             val response = httpClient.get("${baseUrl}/events/${userId}/${policyId}")
 
-            if(response.status == HttpStatusCode.OK)
+            if (response.status == HttpStatusCode.OK)
                 Result.success(response.body())
+            else
+                Result.failure(Exception(response.status.description))
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+    override suspend fun createEvent(
+        userId: String,
+        policy: PolicyDTO,
+        startDate: String,
+        endDate: String,
+        partOfDay: PartOfDayDTO,
+        additionalInfo: String?,
+        includesBreakfast: Boolean?,
+        city: String?,
+        project: ProjectDTO?
+    ): Result<Nothing?> {
+        return try{
+            val body = EventDTO(
+                policy = policy,
+                startDate = startDate,
+                endDate = endDate,
+                partOfDay = partOfDay,
+                includesBreakfast = includesBreakfast,
+                additionalInfo = additionalInfo,
+                city = city,
+                project = project
+            )
+            val response = httpClient.post("${baseUrl}/events/${userId}") {
+                contentType(ContentType.Application.Json)
+                setBody(body)
+            }
+
+            if(response.status == HttpStatusCode.OK)
+                Result.success(null)
             else
                 Result.failure(Exception(response.status.description))
         }catch (e: Exception){
--- shared/src/commonMain/kotlin/com/mindera/minderapeople/apiclient/interfaces/IEventApiClient.kt
+++ shared/src/commonMain/kotlin/com/mindera/minderapeople/apiclient/interfaces/IEventApiClient.kt
@@ -1,8 +1,20 @@
 package com.mindera.minderapeople.apiclient.interfaces
 
 import com.mindera.minderapeople.dto.EventDTO
+import com.mindera.minderapeople.dto.PartOfDayDTO
+import com.mindera.minderapeople.dto.PolicyDTO
+import com.mindera.minderapeople.dto.ProjectDTO
 
 interface IEventApiClient {
     suspend fun getEventById(userId: String, eventId: String): Result<EventDTO>
     suspend fun getEventByPolicy(userId: String, policyId: String): Result<List<EventDTO>>
+    suspend fun createEvent(userId: String,
+                            policy: PolicyDTO,
+                            startDate: String,
+                            endDate: String,
+                            partOfDay: PartOfDayDTO,
+                            additionalInfo: String? = null,
+                            includesBreakfast: Boolean? = null,
+                            city: String? = null,
+                            project: ProjectDTO? = null): Result<Nothing?>
 }
