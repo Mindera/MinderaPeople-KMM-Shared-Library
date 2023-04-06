5d28fd6e4ec714c285a2cae3c19fa334a2b21534
Dani Camelo <dani.camelo@mindera.com>
2023-04-06 14:08:01 +0100

    implemented create Event in Event Repo

--- shared/src/commonMain/kotlin/com/mindera/minderapeople/repository/EventRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/mindera/minderapeople/repository/EventRepositoryImpl.kt
@@ -2,6 +2,9 @@ package com.mindera.minderapeople.repository
 
 import com.mindera.minderapeople.apiclient.interfaces.IEventApiClient
 import com.mindera.minderapeople.dto.EventDTO
+import com.mindera.minderapeople.dto.PartOfDayDTO
+import com.mindera.minderapeople.dto.PolicyDTO
+import com.mindera.minderapeople.dto.ProjectDTO
 import com.mindera.minderapeople.repository.interfaces.IEventRepository
 
 
@@ -12,6 +15,20 @@ class EventRepositoryImpl(val apiClient: IEventApiClient): IEventRepository {
 
     override suspend fun getEventByPolicy(userId: String, policyId: String): Result<List<EventDTO>> {
         return apiClient.getEventByPolicy(userId, policyId)
+    }
+
+    override suspend fun createEvent(
+        userId: String,
+        policy: PolicyDTO,
+        startDate: String,
+        endDate: String,
+        partOfDay: PartOfDayDTO,
+        additionalInfo: String?,
+        includesBreakfast: Boolean?,
+        city: String?,
+        project: ProjectDTO?
+    ): Result<Nothing?> {
+        return apiClient.createEvent(userId, policy, startDate, endDate, partOfDay, additionalInfo, includesBreakfast, city, project)
     }
 
 }
