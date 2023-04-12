42735f3c3e963a381267bc2625125f3998b53df8
Dani Camelo <dani.camelo@mindera.com>
2023-04-12 17:21:28 +0100

    rebase from create new event branch to getProjects branch

--- shared/src/commonMain/kotlin/com/mindera/minderapeople/dto/ProjectDTO.kt
+++ shared/src/commonMain/kotlin/com/mindera/minderapeople/dto/ProjectDTO.kt
@@ -1,6 +1,10 @@
 package com.mindera.minderapeople.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ProjectDTO(val aux:String)
+data class ProjectDTO(
+    @SerialName("id") val id: String? = null,
+    @SerialName("project_name") val projectName: String? = null
+)
