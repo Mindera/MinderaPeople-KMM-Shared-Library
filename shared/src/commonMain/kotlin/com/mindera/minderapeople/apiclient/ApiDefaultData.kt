30d43a4e4a475ff346d1ed91d82b4bc5307f922e
Dani Camelo <dani.camelo@mindera.com>
2023-05-03 14:56:36 +0100

    changed api url to make calls to remote WireMockCloud

--- shared/src/commonMain/kotlin/com/mindera/minderapeople/apiclient/ApiDefaultData.kt
+++ shared/src/commonMain/kotlin/com/mindera/minderapeople/apiclient/ApiDefaultData.kt
@@ -3,7 +3,7 @@ package com.mindera.minderapeople.apiclient
 object ApiDefaultData {
 
     //API URL
-    const val BASE_URL = "http://localhost:3000/api"
+    const val BASE_URL = "https://6l3y1.wiremockapi.cloud/api"
     const val API_IGNORE_UNKNOWN_KEYS = true
 
     //Parameters
