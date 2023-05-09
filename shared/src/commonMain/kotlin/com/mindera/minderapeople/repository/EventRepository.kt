47b11b61934d7ca46669472cdd511297ef6712ed
Dani Camelo <dani.camelo@mindera.com>
2023-05-09 12:48:35 +0100

    removed unnecessary new lines

--- shared/src/commonMain/kotlin/com/mindera/minderapeople/repository/EventRepository.kt
+++ shared/src/commonMain/kotlin/com/mindera/minderapeople/repository/EventRepository.kt
@@ -3,6 +3,7 @@ package com.mindera.minderapeople.repository
 import com.mindera.minderapeople.apiclient.interfaces.IEventApiClient
 import com.mindera.minderapeople.dto.*
 import com.mindera.minderapeople.repository.interfaces.IEventRepository
+
 class EventRepository(private val apiClient: IEventApiClient) : IEventRepository {
     override suspend fun getAllEventsForUser(userId: String): Result<List<EventDTO>> {
         return apiClient.getAllEventsForUser(userId)
