f90c1efec1f6de8fadb203e133adc87682a0d8fd
Dani Camelo <dani.camelo@mindera.com>
2023-06-12 11:58:44 +0100

    updated Mock API URl

--- shared/src/commonMain/kotlin/com/mindera/minderapeople/apiclient/ApiDefaultData.kt
+++ shared/src/commonMain/kotlin/com/mindera/minderapeople/apiclient/ApiDefaultData.kt
@@ -3,7 +3,7 @@ package com.mindera.minderapeople.apiclient
 object ApiDefaultData {
 
     //API URL
-    const val BASE_URL = "https://6l3y1.wiremockapi.cloud/api"
+    const val BASE_URL = "https://6v60j.wiremockapi.cloud/api"
     const val API_IGNORE_UNKNOWN_KEYS = true
 
     //Parameters
